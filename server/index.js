7fdb49ca34c80a24e5ab6c9b505317e49a30a905
Sharmas <82169761+sharmas07@users.noreply.github.com>
2023-08-03 12:53:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const messageModel = require('./models/messageModel')
 
 
 const app=express();
-const port=4000;
+const port= process.env.PORT || 4000;
 const cors = require("cors");
 app.use(cors());
 dotenv.config()
